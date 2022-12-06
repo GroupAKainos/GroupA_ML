1bfa2ede9387482956095781c346166db2f8fb19
aphawkins1 <alexphawkins@hotmail.co.uk>
2022-12-06 11:11:52 +0000

    notebook with instructions on how to delpoy to aws lambda serverless. /formality-api folder which includes all files needed to delpoy to aws

---
+++ formality-api/app/api/__init__.py
@@ -0,0 +1,1 @@
+from .api_inference import inference, load_models
---
+++ formality-api/app/app.py
@@ -0,0 +1,29 @@
+from api.api_inference import inference, load_models
+import json
+
+model_path = "./models/model.pkl"
+vec_path = "./models/vec.pkl"
+
+model, vec = load_models(model_path, vec_path)
+
+def lambda_handler(event, context):
+    text = event['body']
+    print("Input text:", text, type(text))
+
+    sentences, scores, formality = inference(text, vec, model)
+
+    print("Sentences:", sentences)
+    print("Scores:", scores)
+    print("Formality:", formality)
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(
+            {
+                "sentences": sentences,
+                "scores": scores,
+                "formality": formality
+
+            }
+        )
+    }
